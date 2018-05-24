b4fd3b54526f61677cf4abcdd690691c86082451
Sendy <daishihua@sanjiang365.com>
2018-05-24 11:44:25 +0800

    版本更新

--- sql/mysql/mysql.sql
+++ sql/mysql/mysql.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS dashboard_board;
-
 CREATE TABLE dashboard_board (
   board_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(50) NOT NULL,
@@ -8,7 +6,6 @@ CREATE TABLE dashboard_board (
   layout_json text,
   PRIMARY KEY (board_id)
 );
-DROP TABLE IF EXISTS dashboard_category;
 
 CREATE TABLE dashboard_category (
   category_id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -16,7 +13,6 @@ CREATE TABLE dashboard_category (
   user_id varchar(100) NOT NULL,
   PRIMARY KEY (category_id)
 );
-DROP TABLE IF EXISTS dashboard_datasource;
 
 CREATE TABLE dashboard_datasource (
   datasource_id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -26,7 +22,6 @@ CREATE TABLE dashboard_datasource (
   config text,
   PRIMARY KEY (datasource_id)
 );
-DROP TABLE IF EXISTS dashboard_widget;
 
 CREATE TABLE dashboard_widget (
   widget_id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -36,7 +31,6 @@ CREATE TABLE dashboard_widget (
   data_json text,
   PRIMARY KEY (widget_id)
 );
-DROP TABLE IF EXISTS dashboard_dataset;
 
 CREATE TABLE dashboard_dataset (
   dataset_id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -46,7 +40,6 @@ CREATE TABLE dashboard_dataset (
   data_json text,
   PRIMARY KEY (dataset_id)
 );
-DROP TABLE IF EXISTS dashboard_user;
 
 CREATE TABLE dashboard_user (
   user_id varchar(50) NOT NULL,
@@ -60,15 +53,12 @@ CREATE TABLE dashboard_user (
 INSERT INTO dashboard_user (user_id,login_name,user_name,user_password)
 VALUES('1', 'admin', 'Administrator', 'ff9830c42660c1dd1942844f8069b74a');
 
-DROP TABLE IF EXISTS dashboard_user_role;
-
 CREATE TABLE dashboard_user_role (
   user_role_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(100) DEFAULT NULL,
   role_id varchar(100) DEFAULT NULL,
   PRIMARY KEY (user_role_id)
 );
-DROP TABLE IF EXISTS dashboard_role;
 
 CREATE TABLE dashboard_role (
   role_id varchar(100) NOT NULL,
@@ -76,7 +66,6 @@ CREATE TABLE dashboard_role (
   user_id varchar(50) DEFAULT NULL,
   PRIMARY KEY (role_id)
 );
-DROP TABLE IF EXISTS dashboard_role_res;
 
 CREATE TABLE dashboard_role_res (
   role_res_id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -86,7 +75,7 @@ CREATE TABLE dashboard_role_res (
   permission varchar(20) DEFAULT NULL,
   PRIMARY KEY (role_res_id)
 );
-DROP TABLE IF EXISTS dashboard_job;
+
 CREATE TABLE dashboard_job (
   job_id bigint(20) NOT NULL AUTO_INCREMENT,
   job_name varchar(200) DEFAULT NULL,
@@ -102,7 +91,6 @@ CREATE TABLE dashboard_job (
   PRIMARY KEY (job_id)
 );
 
-DROP TABLE IF EXISTS dashboard_board_param;
 CREATE TABLE dashboard_board_param (
   board_param_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(50) NOT NULL,
@@ -123,52 +111,3 @@ ALTER  TABLE  dashboard_widget ADD update_time TIMESTAMP DEFAULT now();
 
 ALTER  TABLE  dashboard_board ADD create_time TIMESTAMP DEFAULT now();
 ALTER  TABLE  dashboard_board ADD update_time TIMESTAMP DEFAULT now();
-
--- 0.5 Real folder
--- Real folder
-DROP TABLE  dashboard_folder;
-CREATE TABLE dashboard_folder (
-  folder_id int PRIMARY KEY AUTO_INCREMENT,
-  folder_name VARCHAR(50),
-  parent_id int DEFAULT -1,
-  is_private int DEFAULT 0,
-  user_id VARCHAR(50) DEFAULT '1',
-  create_time TIMESTAMP DEFAULT now(),
-  update_time TIMESTAMP DEFAULT now()
-);
-ALTER TABLE dashboard_folder AUTO_INCREMENT=10000;
-
--- Root Folder
-INSERT INTO dashboard_folder (folder_id,folder_name,parent_id) VALUES (10000, 'Root', -1);
--- Private Folder
-INSERT INTO dashboard_folder (folder_name, parent_id, is_private) VALUES ('.private', 10000, 1);
--- Dashboard Category Folder
-INSERT INTO dashboard_folder (folder_name, parent_id) SELECT category_name, 10000 FROM dashboard_category;
-
-ALTER TABLE dashboard_dataset ADD COLUMN folder_id INT DEFAULT 10000;
-ALTER TABLE dashboard_widget  ADD COLUMN folder_id INT DEFAULT 10000;
-ALTER TABLE dashboard_board   ADD COLUMN folder_id INT DEFAULT 10000;
-
--- Update board folder
-UPDATE dashboard_board a
-  JOIN dashboard_category c ON a.category_id = c.category_id
-  JOIN dashboard_folder f   ON c.category_name = f.folder_name AND f.parent_id=10000
-   SET a.folder_id = f.folder_id;
-
-UPDATE dashboard_board a
-SET a.folder_id = (SELECT folder_id FROM dashboard_folder WHERE folder_name = '.private' AND parent_id=10000)
-WHERE a.folder_id is NULL;
-
--- add version
-DROP TABLE IF EXISTS Meta_Version;
-CREATE TABLE Meta_Version (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50),
-  status int DEFAULT 0,
-  create_time TIMESTAMP DEFAULT now(),
-  update_time TIMESTAMP DEFAULT now()
-);
-
-INSERT INTO Meta_Version (name) VALUES ('Folder');
-
-
